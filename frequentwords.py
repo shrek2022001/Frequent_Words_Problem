e272f6ad706c7c9aa6e33498970243d1b5332a8e
Shreya <ekandeshreya9@gmail.com>
2022-09-13 17:41:39 +0530

    first commit

---
+++ frequentwords.py
@@ -0,0 +1,31 @@
+def FrequentWords(Text, k):
+    # your code here
+    words = []
+    freq = FrequencyMap(Text, k)
+    m = max(freq.values())
+    for key in freq:
+        if freq[key]==m:
+            words.append(key)
+    return words
+# Copy your FrequencyMap() function here.
+
+
+def FrequencyMap(Text, k):
+    # your code here.
+    freq = {}
+    n = len(Text)
+    for i in range(n-k+1):
+        Pattern = Text[i:i+k]
+        freq[Pattern] = 0
+        for j in range(n-k+1):
+            if Text[j:j+k]==Pattern:
+                freq[Pattern]=freq[Pattern]+1
+    return freq
+
+# Now set Text equal to the Vibrio cholerae oriC and k equal to 10
+#Text='ATCAATGATCAACGTAAGCTTCTAAGCATGATCAAGGTGCTCACACAGTTTATCCACAACCTGAGTGGATGACATCAAGATAGGTCGTTGTATCTCCTTCCTCTCGTACTCTCATGACCACGGAAAGATGATCAAGAGAGGATGATTTCTTGGCCATATCGCAATGAATACTTGTGACTTGTGCTTCCAATTGACATCTTCAGCGCCATATTGCGCTGGCCAAGGTGACGGAGCGGGATTACGAAAGCATGATCATGGCTGTTGTTCTGTTTATCTTGTTTTGACTGAGACTTGTTAGGATAGACGGTTTTTCATCACTGACTAGCCAAAGCCTTACTCTGCCTGACATCGACCGTAAATTGATAATGAATTTACATGCTTCCGCGACGATTTACCTCTTGATCATCGATCCGATTGAAGATCTTCAATTGTTAATTCTCTTGCCTCGACTCATAGCCATGATGAGCTCTTGATCATGTTTCCTTAACCCTCTATTTTTTACGGAAGAATGATCAAGCTGCTGCTCTTGATCATCGTTTC'
+#Text = 'atgaccgggatactgataaaaaaaagggggggggcgtacacattagataaacgtatgaagtacgttagactcggcgccgccgacccctattttttgagcagatttagtgacctggaaaaaaaatttgagtacaaaacttttccgaataaaaaaaaagggggggatgagtatccctgggatgacttaaaaaaaagggggggtgctctcccgatttttgaatatgtaggatcattcgccagggtccgagctgagaattggatgaaaaaaaagggggggtccacgcaatcgcgaaccaacgcggacccaaaggcaagaccgataaaggagatcccttttgcggtaatgtgccgggaggctggttacgtagggaagccctaacggacttaataaaaaaaagggggggcttataggtcaatcatgttcttgtgaatggatttaaaaaaaaggggggggaccgcttggcgcacccaaattcagtgtgggcgagcgcaacggttttggcccttgttagaggcccccgtaaaaaaaagggggggcaattatgagagagctaatctatcgcgtgcgtgttcataacttgagttaaaaaaaagggggggctggggcacatacaagaggagtcttccttatcagttaatgctgtatgacactatgtattggcccattggctaaaagcccaacttgacaaatggaagatagaatccttgcataaaaaaaagggggggaccgaaagggaagctggtgagcaacgacagattcttacgtgcattagctcgcttccggggatctaatagcacgaagcttaaaaaaaaggggggga'
+Text = input("Enter string")
+k=15
+# Finally, print the result of calling FrequentWords on Text and k.
+print(FrequentWords(Text, k))
---
+++ script.js
@@ -0,0 +1,17 @@
+let sequence = document.getElementById('seq')
+let length = document.getElementById('length')
+let button = document.getElementById('button')
+
+
+function FrequentWords() {
+    var words = [];
+    freq = FrequencyMap(this, Text, k)
+
+}
+function FrequencyMap() {
+
+}
+
+
+
+button.addEventListener('click',FrequentWords)
